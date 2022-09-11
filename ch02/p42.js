3567d824c73f8c0ee125974b1d91c5b1f4b47f7a
renat amirov <amirov.renat.1st@gmail.com>
2022-09-11 12:17:33 +0400

    Chapter 2 - page 42

---
+++ ch02/p42.js
@@ -0,0 +1,19 @@
+const π = 3.14;
+const sί = true;
+
+let café = 1;
+
+console.log('π = ' + π);
+
+console.log('café = ' + café);
+
+console.log(caf\u00e9);
+
+console.log(caf\u{E9});
+
+console.log("\u{1F600}");
+
+console.log("cafe\u{301}");
+
+
+
